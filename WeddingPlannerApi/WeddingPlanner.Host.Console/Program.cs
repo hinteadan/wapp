5b907256ae2054394938bb76309c8e3748e7221f
Hintee <dan.hintea@recognos.ro>
2017-05-23 15:15:31 +0300

    Scaffold API

---
+++ WeddingPlannerApi/WeddingPlanner.Host.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingPlanner.Host.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:9969"));
+            host.Start();
+
+            System.Console.WriteLine($"WeddingPlannerApi started @ ${DateTime.Now}");
+            System.Console.WriteLine("Press key to stop");
+            System.Console.ReadKey();
+            host.Stop();
+        }
+    }
+}
---
+++ WeddingPlannerApi/WeddingPlannerApi/PingModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingPlannerApi
+{
+    public class PingModule : NancyModule
+    {
+        public PingModule() : base("/ping")
+        {
+            Get["/"] = _ => Response.AsText($"pong @ {DateTime.Now}");
+        }
+    }
+}
